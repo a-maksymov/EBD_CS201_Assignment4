266f91375b41d613c96e61f6d36486f901fe5c73
maxander <andrii.maksymov.25@ske.org.ua>
2026-07-23 01:59:31 +0300

    some bugs were fixed and little changes was provided for comments

--- Assigment_4.py
+++ Assigment_4.py
@@ -2,15 +2,15 @@ import json
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
-#зчитуємо json
+#зчитую json
 with open('regional_tariffs.json', 'r') as file:
     tariffs = json.load(file)
-#зчитуємо файл типу ЦеВеликіЯйця
+#зчитую файл типу ЦеВеликіЯйця
 with open('global_sales.csv', 'r') as file:
     reader = csv.DictReader(file)
     dict_of_sales = list(reader)
 
-#чистимо словник від N/A
+#чистю словник від N/A
 for key in tariffs:
 
     if tariffs[key] == "N/A":
@@ -27,14 +27,14 @@ for lines in dict_of_sales:
     lines["quantity"] = int(lines["quantity"])
     lines["revenue"] = float(lines["revenue"])
 
-#рахуємо чистий прибуток і додаємо нову колонку
+#рахую чистий прибуток і додаю нову колонку в наш ліст компрехеншн
 for row in dict_of_sales:
     region = row["region"]
     tariff = float(tariffs[region])
-    revenue = row["revenue"]
+    revenue = float(row["revenue"])
     net_profit = revenue - (revenue * (tariff / 100))
     row["net_profit"] = round(net_profit,2)
-#записуємо новий файл
+#записую новий файл
 fieldnames = list(dict_of_sales[0].keys())
 with open('cleaned_sales_updated.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.DictWriter(file, fieldnames=fieldnames)
@@ -42,7 +42,7 @@ with open('cleaned_sales_updated.csv', 'w', newline='', encoding='utf-8') as fil
     writer.writerows(dict_of_sales)
 print("Оновлений ЦеВеликіЯйця.файл успішно збережений до файлу - 'cleaned_sales_updated.csv'")
 
-#сортуємо чистий прибуток по категоріям
+#сортую чистий прибуток по категоріям
 
 profit_in_category = {}
 
@@ -56,20 +56,20 @@ for row in dict_of_sales:
     else:
         profit_in_category[category] = profit
 
-# print("Сумарний прибуток за категоріями:")
+#### print("Сумарний прибуток за категоріями:")
 # print(profit_in_category)
 
-#рахуємо середнє по категоріям
+#рахую середнє по категоріям
 average_profit = sum(profit_in_category.values()) / len(profit_in_category)
-#Використовуємо лямбда фукції для сортування топ категорій
+#використовую лямбда фукцію для сортування топ категорій
 top_categories = dict(filter(lambda item: item[1] > average_profit, profit_in_category.items()))
 
-#Створюємо пустий ліст, як буфер для діставання данних у потрібному порядку(спочатку значення, а тоді категоря, аби можна було нормально відсортувати)
+#Створюю пустий ліст, як буфер для діставання данних у потрібному порядку(спочатку значення, а тоді категоря, аби можна було нормально відсортувати)
 profit_list = []
 for category in top_categories:
     profit = top_categories[category]
     profit_list.append([profit, category])
-#Сортуємо цей буферний ліст
+#Сортую цей буферний ліст
 profit_list.sort(reverse=True)
 #Словник для фінального варіанту
 sorted_top_categories = {}
@@ -81,7 +81,7 @@ for znachennya in profit_list:
 # print(sorted_top_categories)
 
 
-#пишемо це у новий JEsusCrist file
+#пишу це у новий JEsusCrist file
 with open('top_categories.json', 'w', encoding='utf-8') as file:
     json.dump(sorted_top_categories, file, indent=4)
 
@@ -93,9 +93,9 @@ print("Топ категорії успішно збережено у 'top_categ
 # а потім передаємо у датафраме.
 # вказую назви колонок
 df = pd.DataFrame(list(sorted_top_categories.items()), columns=['Product Category', 'Net Profit'])
-# збільшуємо всі індекси на 1
+# збільшую всі індекси на 1
 df.index += 1
-# виводимо красиву таблицю на екран
+# виводю красиву таблицю на екран
 print("\nТаблиця топ категорій:")
 print(df)
 
@@ -105,12 +105,12 @@ profits = list(sorted_top_categories.values())
 # cтворюю чистий листок з конкретними розмірами
 fig, aboba = plt.subplots(figsize=(10, 10))
 
-# малюємо стовпчики
+# малюю стовпчики
 aboba.bar(categories, profits, color="green", edgecolor="black", zorder=8)
 
 # додаю заголовки та підписи через set_
 aboba.set_title("Чистий прибуток за топ-категоріями", fontsize=20, fontweight="bold")
-aboba.set_xlabel("Категорія товарів", fontsize=15)
+aboba.set_xlabel("Категорії товарів", fontsize=15)
 aboba.set_ylabel("Прибуток", fontsize=15)
 
 # сітка
